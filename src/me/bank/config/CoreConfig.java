ba8181f1aac6729c25a9a361572e63cd1b0add60
lxc <ludivicio@gmail.com>
2014-09-12 11:55:42 +0800

    实现登录界面

--- src/me/bank/config/CoreConfig.java
+++ src/me/bank/config/CoreConfig.java
@@ -92,16 +92,10 @@ public class CoreConfig extends JFinalConfig {
 	 * 配置处理器
 	 */
 	public void configHandler(Handlers me) {
-		// me.add(new FakeStaticHandler());
 		me.add(new FakeStaticHandler());
-
 		me.add(new ContextPathHandler());
 	}
 
-	/**
-	 * 建议使用 JFinal 手册推荐的方式启动项�?运行�?main
-	 * 方法可以启动项目，此main方法可以放置在任意的Class类定义中，不�?��要放于此
-	 */
 	public static void main(String[] args) {
 		JFinal.start("WebRoot", 8080, "/", 5);
 	}
